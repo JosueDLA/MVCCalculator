2880494708b1f5e95d6b3e40fd01ef5d70fc6724
Josue <jdeleona10@miumg.edu.gt>
2019-06-21 03:46:23 -0600

    Agregar archivos de proyecto.

---
+++ calculadora/Models/CalculadoraModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace calculadora.Models
+{
+    public class CalculadoraModel
+    {
+        public double A { get; set; }    
+        public double B { get; set; }    
+        public string Result { get; set; }   
+    }
+}
